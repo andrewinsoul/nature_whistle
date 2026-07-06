fc8f6dfa60d1ecab3b39e4028cf227f0bbad356d
Nature <andrewinsoul@gmail.com>
2026-07-06 05:46:25 +0100

    Bump version to 0.3.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule NatureWhistle.MixProject do
   def project do
     [
       app: :nature_whistle,
-      version: "0.2.0",
+      version: "0.3.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
